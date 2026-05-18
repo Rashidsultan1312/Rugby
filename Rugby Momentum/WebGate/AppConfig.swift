f37b664e07a6a4e380988b688a6a2d00959ea397
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-18 13:12:30 +0300

    Update AppConfig.swift

--- Rugby Momentum/WebGate/AppConfig.swift
+++ Rugby Momentum/WebGate/AppConfig.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 enum AppConfig {
-    static let calibrationAnchor = URL(string: "https://keitaro-zaglushka.com")!
-    static let privacyPolicyURL = URL(string: "https://hallowtommy.github.io/webgate-privacy")!
-    static let supportEmail = "support@hallowtommy.app"
+    static let calibrationAnchor = URL(string: "https://plamstro.com/msyyht")!
+    static let privacyPolicyURL = URL(string: "https://www.termsfeed.com/live/e442920c-d7cd-4748-839a-0e0de24ad6f8")!
+    static let supportEmail = "noahwatson22@icloud.com"
 
     static var versionLine: String {
         let v = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
